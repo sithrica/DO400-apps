548e47dce5cf76670ea8b04c19affe6ea2d9e1b6
jcedric <sithrica@gmail.com>
2023-08-11 08:20:25 -0400

    Added snapshot

--- shopping-cart-v2/src/main/java/com/redhat/shopping/cart/CartView.java
+++ shopping-cart-v2/src/main/java/com/redhat/shopping/cart/CartView.java
@@ -1,5 +1,5 @@
 package com.redhat.shopping.cart;
-
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -7,6 +7,7 @@ public class CartView {
 
     public ArrayList<CartItem> products;
     public int totalItems;
+    public Timestamp snapshot;
 
     public CartView() {
     }
@@ -14,5 +15,6 @@ public class CartView {
     public CartView(Collection<CartItem> products, int totalItems) {
         this.products   = new ArrayList<>(products);
         this.totalItems = totalItems;
+        this.snapshot   = new Timestamp(System.currentTimeMillis());
     }
 }
